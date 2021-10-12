678e45fbe730c81c01a1c0d066382bffba1c729e
oleksandr <oleksandrchervinskyi2@gmail.com>
2021-10-12 11:04:22 +0300

    finish feedback

--- src/features/main/feedback/index.js
+++ src/features/main/feedback/index.js
@@ -1,5 +1,5 @@
 import styles from './feedback.module.css'
-import {Carousel, Col, Container, Row} from "react-bootstrap";
+import {Carousel, CarouselItem, Col, Container, Row} from "react-bootstrap";
 import men1 from '../../../app/pictures/main/Men1.png';
 import men2 from '../../../app/pictures/main/Men2.png';
 import men3 from '../../../app/pictures/main/Men3.png';
@@ -22,22 +22,48 @@ export const Feedback = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Carousel>
-                        <Col lg={4}>
-                            <div className={styles.cards}>
-                                <img src={men1} alt="men1"/>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit Ultrices.
-                                </p>
-                                <p>
-                                    David lee
-                                </p>
-                                <p>
-                                    davidlee@
-                                </p>
-                            </div>
-                        </Col>
-                    </Carousel>
+                    <Col lg={4}>
+                        <div className={styles.cards}>
+                            <img src={men1} alt="men1" className={styles.photo}/>
+                            <p className={styles.feedback_text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit Ultrices.
+                            </p>
+                            <p className={styles.name}>
+                                David lee
+                            </p>
+                            <p className={styles.social_link}>
+                                davidlee@
+                            </p>
+                        </div>
+                    </Col>
+                    <Col lg={4}>
+                        <div className={styles.cards}>
+                            <img src={men3} alt="men1" className={styles.photo}/>
+                            <p className={styles.feedback_text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit Ultrices.
+                            </p>
+                            <p className={styles.name}>
+                                Ravi shankor
+                            </p>
+                            <p className={styles.social_link}>
+                                ravishankor@
+                            </p>
+                        </div>
+                    </Col>
+                    <Col lg={4}>
+                        <div className={styles.cards}>
+                            <img src={men2} alt="men3" className={styles.photo}/>
+                            <p className={styles.feedback_text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit Ultrices.
+                            </p>
+                            <p className={styles.name}>
+                                Thomas lew
+                            </p>
+                            <p className={styles.social_link}>
+                                thomaslew@
+                            </p>
+                        </div>
+                    </Col>
                 </Row>
             </Container>
         </section>
